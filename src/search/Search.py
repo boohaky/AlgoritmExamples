93b92217f19ca4c9f74ec626fa87164f48228c4b
Kharchenko Kyrylo <kyrylo.kharchenko@globallogic.com>
2018-11-26 11:16:29 +0200

    add bfs and dfs searches

--- src/search/Search.py
+++ src/search/Search.py
@@ -21,11 +21,71 @@ def binarySearch(list_to_search, element):
         elif element > list_to_search[mid]:
             low = mid + 1
         else:
-            return "Binary Search index " + str(mid);
+            return "Binary Search index " + str(mid)
     else:
-        return "Binary Search " + "no number";
+        return "Binary Search " + "no number"
+
+
+def pickNextTop(graph, visited_tops):
+    for i in range(len(graph[0])):
+        if i not in visited_tops:
+            return i
+    return -1
+
+
+class DFS:
+
+    def search(self, graph, value):
+        graph_length = len(graph)
+        graph_value = 0
+        visited_tops = []
+        while graph_value != value and len(visited_tops) < graph_length:
+            visited_tops.append(graph_value)
+            graph_value = self.dfs(graph, graph_value, visited_tops)
+            if graph_value == -1:
+                graph_value = pickNextTop(graph, visited_tops)
+        if graph_value != value:
+            return str(value) + " not found"
+        else:
+            return str(value) + " found"
+
+    def dfs(self, graph, top, visited_tops):
+        for i in range(len(graph[top])):
+            if graph[top][i] != 0 and i not in visited_tops:
+                return i
+        return -1
+
+
+class BFS:
+
+    def search(self, graph, value):
+        graph_length = len(graph)
+        graph_value = 0
+        visited_tops = [graph_value]
+        while graph_value != value and len(visited_tops) < graph_length:
+            graph_value = self.bfs(graph, value, graph_value, visited_tops)
+            if graph_value == -1:
+                graph_value = pickNextTop(graph, visited_tops)
+        if graph_value != value:
+            return str(value) + " not found"
+        else:
+            return str(value) + " found"
+
+    def bfs(self, graph, searched_value, top, visited_tops):
+        for i in range(len(graph[top])):
+            if graph[top][i] != 0:
+                visited_tops.append(i)
+                if i == searched_value:
+                    return i
+        return -1
 
 
 if __name__ == '__main__':
     print(maxElement([45, 20, 5, 9, 6]))
     print(binarySearch([5, 6, 9, 45, 150, 180], 5))
+    graph = [[0, 1, 1, 0],
+             [1, 0, 0, 1],
+             [1, 0, 0, 0],
+             [0, 1, 0, 0]]
+    print(DFS().search(graph, 3))
+    print(BFS().search(graph, 3))
